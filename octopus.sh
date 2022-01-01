97f982a1fe2d9383747b1c3415003c215c7d07df
Vrublevsky Vitaliy <vrublevsky@mindbox.ru>
2022-01-01 15:33:56 +0300

    worked version

--- octopus.sh
+++ octopus.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
-commitMessage=$(git log -1 --pretty=oneline)
-echo $commitMessage
 commits=$(echo $(git log -n 10 --pretty=format:'{%n  "Id": "%h",%n   "Comment": "%f"%n},' $@ | perl -pe 'BEGIN{print "["}; END{print "]\n"}' |  perl -pe 's/},]/}]/'))
+# echo $commits
 jsonBody=$(jq -nr --arg v "${commits}" '{
         "BuildEnvironment":"BitBucket",
         "Branch":"main",
@@ -13,28 +11,16 @@ jsonBody=$(jq -nr --arg v "${commits}" '{
         "VcsCommitNumber":"314cf2c3ee916c92a384c2796a6abe332d678e4f",
         "Commits": $v
         }')
-echo $jsonBody
-echo "========================="
-jsonBody=$(jq \
-          --arg commits "$comma" \
-    '{
-        "BuildEnvironment":"BitBucket",
-        "Branch":"main",
-        "BuildNumber":"288",
-        "BuildUrl":"https://bitbucket.org/octopussamples/petclinic/addon/pipelines/home#!/results/288",
-        "VcsType":"Git",
-        "VcsRoot":"http://bitbucket.org/octopussamples/petclinic",
-        "VcsCommitNumber":"314cf2c3ee916c92a384c2796a6abe332d678e4f",
-        "Commits": $commits
-        }')
-echo $jsonBody > buildinfo.json
-echo "=========="
-cat buildinfo.json
-echo $jsonBody
-# docker run --rm -v $(pwd):/src  octopusdeploy/octo build-information \
-#        --package-id="PetClinic.web" \
-#        --version="1.0.200802.1002" \
-#        --file=buildinfo.json \
-#        --server= \
-#        --apiKey= \
-#        --overwrite-mode=OverwriteExisting # DELETE THIS
+echo ${jsonBody} > tmp.json
+clear=$(cat tmp.json | sed 's/\\"/\"/g')
+tt=${clear/\"[{/\[{}
+ttt=${tt/\}\]\"/\}\]}
+echo $ttt | jq .
+echo $ttt > buildinfo.json
+docker run --rm -v $(pwd):/src  octopusdeploy/octo build-information \
+       --package-id="PetClinic.web" \
+       --version="1.0.200802.1002" \
+       --file=buildinfo.json \
+       --server=http://95.131.25.55:8080 \
+       --apiKey=API-XVB8GHTWSLP0IP0P1XFRAPQ0H9SQPUCM \
+       --overwrite-mode=OverwriteExisting # DELETE THIS
